595773fbd4207c62f337161dd1813d132f614c87
Евгений <leychenkoev@gmail.com>
2019-03-21 13:52:31 +0200

    added Xcode Templates added gitignore added switlint.yml

---
+++ Templates/MVP/Module.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+
+    private var presenter: ___VARIABLE_fileName:identifier___Output?
+
+    // MARK: - Lifecycle -
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - ___VARIABLE_fileName:identifier___Interface -
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_fileName:identifier___Interface {
+    
+}
